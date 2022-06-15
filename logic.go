81748c7cca40ead7f5893404b43b438a6c29b551
mr_destructive <gormeet711@gmail.com>
2022-06-15 21:36:14 +0530

    init

---
+++ logic.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"log"
+	"math/rand"
+)
+
+func info() BattlesnakeInfoResponse {
+	log.Println("INFO")
+	return BattlesnakeInfoResponse{
+		APIVersion: "1",
+		Author:     "mr_destructive",       
+		Color:      "#000030", 
+		Head:       "pixel",
+		Tail:       "pixel", 
+    }
+}
+
+func start(state GameState) {
+    log.Println(state)
+	log.Printf("%s START\n", state.Game.ID)
+}
+
+func end(state GameState) {
+	log.Printf("%s END\n\n", state.Game.ID)
+}
+
+func move(state GameState) BattlesnakeMoveResponse {
+	possibleMoves := map[string]bool{
+		"up":    true,
+		"down":  true,
+		"left":  true,
+		"right": true,
+	}
+
+
+	myHead := state.You.Body[0] 
+	myNeck := state.You.Body[1] 
+	if myNeck.X < myHead.X {
+		possibleMoves["left"] = false
+	} else if myNeck.X > myHead.X {
+		possibleMoves["right"] = false
+	} else if myNeck.Y < myHead.Y {
+		possibleMoves["down"] = false
+	} else if myNeck.Y > myHead.Y {
+		possibleMoves["up"] = false
+	}
+
+	// TODO: Step 1 - Don't hit walls.
+	// Use information in GameState to prevent your Battlesnake from moving beyond the boundaries of the board.
+	boardWidth := state.Board.Width
+	boardHeight := state.Board.Height
+
+    if myNeck.X == boardWidth {
+        possibleMoves["right"] = false
+    }
+    if myNeck.X == 0 {
+        possibleMoves["left"] = false
+    }
+    if myNeck.Y == boardHeight {
+        possibleMoves["down"] = false
+    }
+    if myNeck.Y == 0 {
+        possibleMoves["up"] = false
+    }
+    
+
+	// TODO: Step 2 - Don't hit yourself.
+	// Use information in GameState to prevent your Battlesnake from colliding with itself.
+	// mybody := state.You.Body
+
+	// TODO: Step 3 - Don't collide with others.
+	// Use information in GameState to prevent your Battlesnake from colliding with others.
+
+	// TODO: Step 4 - Find food.
+	// Use information in GameState to seek out and find food.
+
+	// Finally, choose a move from the available safe moves.
+	// TODO: Step 5 - Select a move to make based on strategy, rather than random.
+	var nextMove string
+
+	safeMoves := []string{}
+	for move, isSafe := range possibleMoves {
+		if isSafe {
+			safeMoves = append(safeMoves, move)
+		}
+	}
+
+	if len(safeMoves) == 0 {
+		nextMove = "down"
+		log.Printf("%s MOVE %d: No safe moves detected! Moving %s\n", state.Game.ID, state.Turn, nextMove)
+	} else {
+		nextMove = safeMoves[rand.Intn(len(safeMoves))]
+		log.Printf("%s MOVE %d: %s\n", state.Game.ID, state.Turn, nextMove)
+	}
+    nextMove = "down"
+	return BattlesnakeMoveResponse{
+		Move: nextMove,
+	}
+}
